ac2f8dd573a33dea3a9eae9046e53aeabd347f03
Dominik Mueller <domi89@pcpool203.ipsp.uni-hohenheim.de>
2016-09-21 16:46:50 +0200

    added README and MANIFEST

--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@
 from setuptools import setup, find_packages
 
 
-#with open('README.rst') as f:
-#    readme = f.read()
-#
 #with open('LICENSE') as f:
 #    license = f.read()
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(
     name='simbreed',
     version='0.1',
-    description='Package for simulation in plant breeding.',
-#    long_description=readme,
+    description='Package for simulations in plant breeding.',
+    long_description=readme(),
     author='Dominik Mueller',
-    author_email='Dominik_Mueller@uni-hohenheim.de',
-    url='https://github.com/X',
+    author_email='dominikmueller64@yahoo.de',
+    url='https://github.com/DominikMueller64/Simbreed',
     license='GPLv3',
-    packages=find_packages(exclude=('tests', 'docs', 'profiling', 'graveyard'))
+    packages=find_packages(exclude=('tests', 'docs', 'profiling')),
+    install_requires=['uuid',
+                      'shortuuid',
+                      'sortedcollections',
+                      'sortedcontainers'],
+    include_package_data=True,
+    zip_safe=False
 )
